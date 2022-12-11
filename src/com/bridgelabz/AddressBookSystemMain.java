a12a25557d45153a37741721ff9da0dfc0edc791
Shubham Raj <shubhamaryan805@gmail.com>
2022-12-11 19:27:59 +0530

    Added the welcome message

---
+++ src/com/bridgelabz/AddressBookSystemMain.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class AddressBookSystemMain {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to address book system using collection");
+    }
+}
