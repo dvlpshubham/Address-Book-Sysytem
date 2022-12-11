92b63862eebb1fff30218c4a91b69317f87c0f9c
Shubham Raj <shubhamaryan805@gmail.com>
2022-12-11 19:34:54 +0530

    Added the add contact to address book

--- src/com/bridgelabz/AddressBookSystemMain.java
+++ src/com/bridgelabz/AddressBookSystemMain.java
@@ -1,8 +1,12 @@
 package com.bridgelabz;
-
 public class AddressBookSystemMain {
-
     public static void main(String[] args) {
+
         System.out.println("Welcome to address book system using collection");
+
+        AddressBookSystemDetails details = new AddressBookSystemDetails();
+
+        details.addDetails();
+        details.display();
     }
 }
