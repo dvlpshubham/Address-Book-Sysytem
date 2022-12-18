46486792ff08b70194e4ce3c665ae44fe40e371f
Shubham Raj <shubhamaryan805@gmail.com>
2022-12-18 22:08:46 +0530

    Added ability to get number of person count by city or state.

--- src/com/bridgelabz/AddressBookSystemMain.java
+++ src/com/bridgelabz/AddressBookSystemMain.java
@@ -12,7 +12,7 @@ public class AddressBookSystemMain {
 
         while (true) {
             System.out.println("\nWelcome to Address Book System");
-            System.out.println("1. New Address Book \n2. Select Address Book \n3. Delete Address Book \n4. Search Contact Data \n5. Exit");
+            System.out.println("1. New Address Book \n2. Select Address Book \n3. Delete Address Book \n4. Search Contact Data \n5.View Contact Data \n6.Count Contacts\n7. Exit");
             System.out.print("Enter Your choice: ");
             int choice = sc.nextInt();
             sc.nextLine();
@@ -21,14 +21,14 @@ public class AddressBookSystemMain {
                     System.out.println("Enter Name of new Address Book: ");
                     String bookName = sc.next();
                     sc.nextLine();
-                    addressBookMap.put(bookName, new AddressBookSystemDetails());// adding bookname as a key and vlue is allocating
-                    // memory for addressbook obj
-                    AddressBookSystemDetails.addressBookOptions(addressBookMap.get(bookName));// call addressbookoption method with
+                    addressBookMap.put(bookName, new AddressBookSystemDetails());// adding bookName as a key and value is allocating
+                    // memory for address book obj
+                    AddressBookSystemDetails.addressBookOptions(addressBookMap.get(bookName));// call address book option method with
                     // passing key of hashmap
                     break;
                 case 2:
                     System.out.println("List of available Address Book : ");
-                    Set<String> keys = addressBookMap.keySet();// retrived keys from hashmap to show addressbooklist
+                    Set<String> keys = addressBookMap.keySet();// retrived keys from hashmap to show address book list
                     Iterator<String> i = keys.iterator();
                     while (i.hasNext()) {
                         System.out.println(i.next());
@@ -47,6 +47,14 @@ public class AddressBookSystemMain {
                     System.out.println("Welcome to the search option:");
                     addressBook.searchByOptions();
                 case 5:
+                    System.out.println("Welcome to view By Option:");
+                    addressBook.viewByOption(addressBookMap);
+                    break;
+                case 6:
+                    System.out.println("Welcome to the counter");
+                    addressBook.countByOption();
+                    break;
+                case 7:
                     sc.close();// for closing the programme
                     return;
                 default:
